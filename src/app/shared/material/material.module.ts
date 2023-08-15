259595b227227fee650aaf71493da4c12c0b8f41
@ttulegenov <ttulegenov@rit-it.com>
2023-08-15 13:44:08 +0600

    create material module

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTableModule } from '@angular/material/table';
 
 const modules = [
   MatButtonModule,
   MatTableModule,
+  MatIconModule,
   FormsModule,
-  ReactiveFormsModule
+  ReactiveFormsModule,
 ]
 
 @NgModule({
